068305ff0eadade678fd700a291a787fccc5b717
Carl Hartmeyer <cehartmeyer01@hw.bvsd>
2024-01-23 15:51:00 -0700

    more progress on i/o to subprocess

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import subprocess
 import os
 import re
 import shutil
+import tempfile
 
 assignmentPath = "FacsimileAssignment.zip"
 
 #TODO: actually read test cases from file
 testCases = { #input : expected output
-    1 : 2
+    1 : 2,
+    2 : 3,
+    5 : 6,
+    7 : 8
     }
 
 #Unzip assignment
@@ -16,18 +20,20 @@ if (not os.path.exists("Temp")):
     #this folder will be the destination of extracted files from the assignment
 subprocess.run("tar -xf "+assignmentPath+" -C Temp") #unzip assignment into temp
 
-def runPythonFile(filePath):
-    outs = []
-    print("Running "+filePath)
-    runningFile = subprocess.Popen("py \""+filePath+"\"", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    for valueIn in testCases:
-        result, ignore = runningFile.communicate("1\n")
-        #this may close stdin after sending, which is bad
-        #try accessing stdin directly instead?
-        outs.append(result) #TODO: replace this to actually analyze later
-    runningFile.terminate() #when we're out of test cases, we're done
-    print(outs)
-        
+def runPythonTests(filePath):
+    #somehow, all my attempts to give input are causing eoferror
+    #apparently input raises this whenever it hits eof, but it shouldn't be hitting that until processing all of my test cases
+    
+    #use a temporary file as the input stream to the subprocess
+    with tempfile.TemporaryFile() as inputBuffer:
+        #write all test cases to the input stream
+        for item in testCases:
+            inputBuffer.write(bytes(str(item), "UTF-8")) #files being tested are probably using input() which requests a string, but stdin expects bytes to be written to it
+        #run the subprocess, with stdin as the input stream
+        runningFile = subprocess.Popen("py \""+filePath+"\"", stdin=inputBuffer, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        #capture stdout into a new temp file
+        print("Subprocess: " + str(runningFile.stdout.read())) #TODO: replace with actual read
+        runningFile.terminate() #when we're out of test cases, we're done
 
 for studentFolder in os.scandir("Temp"):
     #print(studentFolder)
@@ -44,7 +50,7 @@ for studentFolder in os.scandir("Temp"):
         if (re.match(".*\\.py", file.name)): #must escape backslash to allow it to appear in regex string
             pythonFiles.append(file)
     if (0 < len(pythonFiles) < 2):
-        runPythonFile(os.path.abspath(pythonFiles[0]))
+        runPythonTests(os.path.abspath(pythonFiles[0]))
     else:
         print("Could not run project in "+studentFolder.name)
         print(str(len(pythonFiles))+" python files in "+studentFolder.name+"/"+latestRevision.name)
