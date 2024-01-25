c25c26b60a3e3300f241c000b48f15af972e2579
Carl Hartmeyer <cehartmeyer01@hw.bvsd>
2024-01-25 13:50:12 -0700

    replace manual creation and destruction of temp folder with tempfile.TemporaryDirectory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 import re
-import shutil
-#import tempfile
+#import shutil
+import tempfile
 #import io
 
 assignmentPath = "FacsimileAssignment.zip"
@@ -14,12 +14,6 @@ testCases = { #input : expected output
     5 : 6,
     7 : 8
     }
-
-#Unzip assignment
-if (not os.path.exists("Temp")):
-    os.mkdir("Temp") #make the temp folder, if it doesn't exist already
-    #this folder will be the destination of extracted files from the assignment
-subprocess.run("tar -xf "+assignmentPath+" -C Temp") #unzip assignment into temp
 
 def runPythonTests(filePath):
     #somehow, all my attempts to give input are causing eoferror
@@ -34,27 +28,29 @@ def runPythonTests(filePath):
     output = runningFile.communicate(inputBuffer) #will be a tuple, with the second value as None
     #anything using normal python input (which is most things) will throw EOFError after all test cases have been read; this is probably fine
     print(output[0]) #TODO: replace with actual read
+
+with tempfile.TemporaryDirectory() as Temp:
+    #Unzip assignment
+    subprocess.run("tar -xf "+assignmentPath+" -C "+os.path.abspath(Temp))
     
-for studentFolder in os.scandir("Temp"):
-    #print(studentFolder)
-    #find the most recent revision
-    latestRevision = None
-    for revisionFolder in os.scandir(studentFolder):
-        #second element will always be revision number ("Revision 1 - On time")
-        if ((latestRevision == None) or (int(revisionFolder.name.split()[1]) > latestRevision.name.split()[1])):  
-            latestRevision = revisionFolder
-    #get the file inside the folder
-    pythonFiles = []
-    for file in os.scandir(latestRevision):
-        print(file.name)
-        if (re.match(".*\\.py", file.name)): #must escape backslash to allow it to appear in regex string
-            pythonFiles.append(file)
-    if (0 < len(pythonFiles) < 2):
-        runPythonTests(os.path.abspath(pythonFiles[0]))
-    else:
-        print("Could not run project in "+studentFolder.name)
-        print(str(len(pythonFiles))+" python files in "+studentFolder.name+"/"+latestRevision.name)
-        #TODO: add interface to specify which file is desired
+    for studentFolder in os.scandir(Temp):
+        #find the most recent revision
+        latestRevision = None
+        for revisionFolder in os.scandir(studentFolder):
+            #second element will always be revision number ("Revision 1 - On time")
+            if ((latestRevision == None) or (int(revisionFolder.name.split()[1]) > latestRevision.name.split()[1])):  
+                latestRevision = revisionFolder
+        #get the file inside the folder
+        pythonFiles = []
+        for file in os.scandir(latestRevision):
+            if (re.match(".*\\.py", file.name)): #must escape backslash to allow it to appear in regex string
+                pythonFiles.append(file)
+        if (0 < len(pythonFiles) < 2):
+            runPythonTests(os.path.abspath(pythonFiles[0]))
+        else:
+            print("Could not run project in "+studentFolder.name)
+            print(str(len(pythonFiles))+" python files in "+studentFolder.name+"/"+latestRevision.name)
+            #TODO: add interface to specify which file is desired
     
 
-shutil.rmtree("Temp")
+#shutil.rmtree("Temp")
