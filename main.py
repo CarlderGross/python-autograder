ac75195fb0442abbca8fe2c5abed9d3c85b2c538
Carl Hartmeyer <cehartmeyer01@hw.bvsd>
2024-02-01 14:25:50 -0700

    added csv summary

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import re
 import tempfile
+import csv
 from tkinter import Tk, filedialog
 
 Tk().withdraw() #temporary: don't create a full window for now
@@ -49,62 +50,80 @@ def inputPromptPattern(file): #parses a python file, and returns a regex pattern
     promptPattern = promptPattern[0:len(promptPattern)-1] #delete the last extraneous separator
     return promptPattern
 
-def processResults(results):
-    #identify which parts are the hardcoded prompts and which parts are the actual output
-    promptPattern = inputPromptPattern(file)
-    for index, result in enumerate(results):
-        expectedOut = list(testCases.values())[index]
-        if (promptPattern):
-            result = re.split(promptPattern, result) #split the string along all known input prompts, leaving only the outputs
-            for i, item in enumerate(result): #clean the results of excess newlines
-                result[i] = item.strip()
-            tup_result = tuple(result[1:len(expectedOut)+1])
-            print(expectedOut)
-            print(tup_result)
-            if (expectedOut == tup_result):
-                print("Success!")
-            else:
-                if (len(expectedOut) == len(tup_result)):
-                    failedFlags = len(expectedOut)
-                    for i, element in enumerate(expectedOut):
-                        if (element in tup_result[i]):
-                            failedFlags -= 1
-                    assert failedFlags >= 0
-                    if (failedFlags == 0):
-                        print("Flagged!")
-                    else:
-                        print("Fail!")
-                else:
-                    print("Fail!")
-
-with tempfile.TemporaryDirectory() as Temp:
-    #Unzip assignment
-    subprocess.run("tar -xf "+assignmentPath+" -C "+os.path.abspath(Temp))
+#use a constant for ease of later editing
+SUMMARYTEMPLATE = {
+    "Name" : None,
+    "Passed" : 0,
+    "Failed" : 0,
+    "Total" : len(testCases),
+    "Flagged" : 0
+    }
+#TODO: make a new csv file each time the program is run
+with open("summary.csv", "w", newline="") as summaryFile:
+    summaryWriter = csv.DictWriter(summaryFile, fieldnames=SUMMARYTEMPLATE.keys())
+    summaryWriter.writeheader()
     
-    for studentFolder in os.scandir(Temp):
-        #find the most recent revision
-        latestRevision = None
-        for revisionFolder in os.scandir(studentFolder):
-            #second element will always be revision number ("Revision 1 - On time")
-            if ((latestRevision == None) or (int(revisionFolder.name.split()[1]) > latestRevision.name.split()[1])):  
-                latestRevision = revisionFolder
-        #get the file inside the folder
-        pythonFiles = []
-        results = None
-        for file in os.scandir(latestRevision):
-            if (re.match(".*\\.py", file.name)): #must escape backslash to allow it to appear in regex string
-                pythonFiles.append(file)
-        if (0 < len(pythonFiles) < 2):
-            results = runPythonTests(os.path.abspath(pythonFiles[0]))
-        else:
-            for file in pythonFiles:
-                if (re.match("[Mm]ain\\.py")): #preferentially run main.py
-                    results = runPythonTests(os.path.abspath(file))
-                    break
-                #TODO: also identify projectname.py
-            if (not results): #if results is still none, since none is falsey
-                print("Could not identify main file in "+studentFolder.name)
-                print(str(len(pythonFiles))+" python files in "+studentFolder.name+"/"+latestRevision.name)
-                #TODO: add interface to specify which file is desired
-        processResults(results)
-        #TODO: provide results to .csv file
+    with tempfile.TemporaryDirectory() as Temp:
+        #Unzip assignment
+        subprocess.run("tar -xf "+assignmentPath+" -C "+os.path.abspath(Temp))
+        
+        for studentFolder in os.scandir(Temp):
+            
+            #obtain the student name and initialize the summary dict
+            studentName = "".join(studentFolder.name.split(" ", 2)[:2])
+            summary = SUMMARYTEMPLATE.copy()
+            summary["Name"] = studentName
+            
+            #find the most recent revision
+            latestRevision = None
+            for revisionFolder in os.scandir(studentFolder):
+                #second element will always be revision number ("Revision 1 - On time")
+                if ((latestRevision == None) or (int(revisionFolder.name.split()[1]) > latestRevision.name.split()[1])):  
+                    latestRevision = revisionFolder
+            #get the file inside the folder
+            pythonFiles = []
+            results = None
+            for file in os.scandir(latestRevision):
+                if (re.match(".*\\.py", file.name)): #must escape backslash to allow it to appear in regex string
+                    pythonFiles.append(file)
+            if (0 < len(pythonFiles) < 2):
+                results = runPythonTests(os.path.abspath(pythonFiles[0]))
+            else:
+                for file in pythonFiles:
+                    if (re.match("[Mm]ain\\.py")): #preferentially run main.py
+                        results = runPythonTests(os.path.abspath(file))
+                        break
+                    #TODO: also identify projectname.py
+                if (not results): #if results is still none, since none is falsey
+                    print("Could not identify main file in "+studentFolder.name)
+                    print(str(len(pythonFiles))+" python files in "+studentFolder.name+"/"+latestRevision.name)
+                    #TODO: add interface to specify which file is desired
+    
+            promptPattern = inputPromptPattern(file)
+            
+            for index, result in enumerate(results):
+                expectedOut = list(testCases.values())[index]
+                #separate hardcoded prompts from actual output
+                if (promptPattern):
+                    result = re.split(promptPattern, result) #split the string along all known input prompts, leaving only the outputs
+                    for i, item in enumerate(result): #clean the results of excess newlines
+                        result[i] = item.strip()
+                #use tuple comparison to identify passed or failed cases
+                tup_result = tuple(result[1:len(expectedOut)+1])
+                print(expectedOut)
+                print(tup_result)
+                if (expectedOut == tup_result):
+                    summary["Passed"] += 1
+                else:
+                    if (len(expectedOut) == len(tup_result)):
+                        failedElements = len(expectedOut)
+                        for i, element in enumerate(expectedOut):
+                            if (element in tup_result[i]):
+                                failedElements -= 1
+                        assert failedElements >= 0
+                        if (failedElements == 0):
+                            summary["Flagged"] += 1
+                            #TODO: write this to flagged outputs file
+                    summary["Failed"] += 1
+            #write summary to CSV file
+            summaryWriter.writerow(summary)
